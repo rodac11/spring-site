89942fb54f4830f845f267712a57fa3557ae10e1
rodac11 <r.aguilera.croasdaile@gmail.com>
2024-06-16 14:10:42 -0400

    Initializer files

---
+++ src/test/java/com/boot/myline/MylineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.myline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
